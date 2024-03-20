7fa7ecb9f4966d0e794de46cf84b899816847f7e
mrony <marie.rony@gmail.com>
2024-03-20 12:51:08 +0100

    🚀 Implement the sign up and log in pages in the single page app struct

--- srcs/static/spa_router.js
+++ srcs/static/spa_router.js
@@ -3,7 +3,26 @@ function route(path) {
 	handleLocation();
 };
 
+function is_log() {
+	return false;
+}
+
 async function handleLocation() {
+	if (!is_log()) {
+		document.querySelector("header").innerHTML = await fetch("/static/header.html").then(response => response.text());
+		document.querySelector("footer").innerHTML = await fetch("/static/footer.html").then(response => response.text());
+		if (window.location.pathname === "/signup")
+		{
+			document.querySelector("main").innerHTML = await fetch("/static/signup.html").then(response => response.text());
+		}
+		else if (window.location.pathname === "/signin")
+		{
+			document.querySelector("main").innerHTML = await fetch("/static/signin.html").then(response => response.text());
+		}
+	}
+	else {
+		//
+	}
 	// @TODO dynamic body content
 };
 
