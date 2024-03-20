68324a3ef18a06033f4c07cd05517dd6fafc60f2
mrony <marie.rony@gmail.com>
2024-03-20 15:48:05 +0100

    🚀 Implement first view calling the db and rendering it in the html + luffy pointer

--- srcs/srcs/urls.py
+++ srcs/srcs/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import SPA_view
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
+	path('api/user_management/', include('user_management.urls')),
 	path('', SPA_view.SPAView.as_view(), name='base'),
 	path('<path:test>', SPA_view.SPAView.as_view(), name='base'),
 ]
--- srcs/static/spa_router.js
+++ srcs/static/spa_router.js
@@ -7,6 +7,18 @@ function is_log() {
 	return false;
 }
 
+async function getMyProfile() {
+	let html = fetch("/static/me.html").then(response => response.text());
+	let user = fetch("/api/user_management/me").then(response => response.json());
+
+	let [main, user_data] = await Promise.all([html, user]);
+
+	main = main.replace("{{username}}", user_data.username);
+	main = main.replace("{{email}}", user_data.email);
+
+	document.querySelector("main").innerHTML = main;
+}
+
 async function handleLocation() {
 	if (!is_log()) {
 		document.querySelector("header").innerHTML = await fetch("/static/header.html").then(response => response.text());
@@ -15,9 +27,16 @@ async function handleLocation() {
 		{
 			document.querySelector("main").innerHTML = await fetch("/static/signup.html").then(response => response.text());
 		}
-		else if (window.location.pathname === "/signin")
+		else if (window.location.pathname === "/login")
 		{
-			document.querySelector("main").innerHTML = await fetch("/static/signin.html").then(response => response.text());
+			document.querySelector("main").innerHTML = await fetch("/static/login.html").then(response => response.text());
+		}
+		else if (window.location.pathname === "/me")
+		{
+			getMyProfile();
+		}
+		else {
+			document.querySelector("main").innerHTML = await fetch("/static/home.html").then(response => response.text());
 		}
 	}
 	else {
@@ -33,3 +52,5 @@ document.addEventListener("DOMContentLoaded", function () {
 		handleLocation();
 	};
 });
+
+window.route = route;
