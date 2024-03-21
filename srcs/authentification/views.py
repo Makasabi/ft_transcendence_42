915e87773fd48b9fcd877923b24501794aa6cb34
Emile ORER <eorer@bess-f4r3s10.clusters.42paris.fr>
2024-03-21 15:17:18 +0100

    Update : signin & signout + prout

--- srcs/authentification/views.py
+++ srcs/authentification/views.py
@@ -1,30 +1,34 @@
-from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer
 
 @api_view(['POST'])
 def login(request):
 	try:
 		user = User.objects.get(username=request.data['username'])
-		if user.check_password(request.data['password']):
-			return Response({"message" : "OK", 'exist' : True})
-		return Response({"message" : "KO", 'exist' : True}, status=400)
+		if not user.check_password(request.data['password']):
+			return Response({"error" : "Wrong password"}, status=status.HTTP_400_BAD_REQUEST)
+		token, created = Token.objects.get_or_create(user=user)
+		serializer = UserSerializer(instance=user)
+		return Response({ "token" : token.key, "user" : serializer.data }, status=status.HTTP_200_OK)
 	except User.DoesNotExist:
-		return Response({ 'message' : "KO", "exist" : False }, status=400)
+		return Response({ "error" : "User does not exist"}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def signup(request):
-	try:
-		new_user = User.objects.create_user(username=request.data['username'], password=request.data['password'], email=request.data['email'])
-		return Response({"message" : "OK"})
-	except ValidationError as e:
-		return Response({ "error" : e}, status=400)
-	except IntegrityError as e:
-		return Response({"error" : "Username already in use"}, status=400)
+    serializer = UserSerializer(data=request.data)
+    if not (serializer.is_valid()):
+        return Response({"error" : serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    serializer.save()
+    user = User.objects.get(username=request.data["username"])
+    token = Token.objects.create(user=user)
+    return Response({"token" : token.key, "user" : serializer.data}, status=status.HTTP_201_CREATED)
+
 
 def index():
 	return HttpResponse("HELLO")
