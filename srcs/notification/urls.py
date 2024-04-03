de5d9b73e66ce398d817770cffa11683ff95aebb
Tiffany GIBIER <imjustwancontact@gmail.com>
2024-04-03 18:33:58 +0200

    🚀 Implement create_notif with api call to user_mgt

--- srcs/notification/urls.py
+++ srcs/notification/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# path("notif_add_friend/<str:username>", views.notif_add_friend, name="notif_add_friend"),
 	path("create_notif/<str:type>/<str:target>", views.create_notif, name="create_notif"),
-	# path("notif_remove_friend/<str:username>", views.notif_remove_friend, name="notif_remove_friend"),
 	path("get_notifs/", views.get_notifs, name="get_notifs"),
 	path('', views.index, name='index'),
 ]
