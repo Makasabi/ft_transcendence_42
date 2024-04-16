fed66e9c4d0e0c995f21acaefc59a0c6a6588ba1
Tiffany GIBIER <imjustwancontact@gmail.com>
2024-04-16 10:18:54 +0200

    🔋 Add apis for user_mgt

--- srcs/front/pages/apis.js
+++ srcs/front/pages/apis.js
@@ -10,16 +10,25 @@ const googleAPI = "/api/auth/google_auth/"; // static method
 const forty2API = "/api/auth/forty2_auth/"; // static method
 const isRegisteredAPI = "/api/auth/is_registered/"; // static method
 
+
 // User management
 const meAPI = "/api/user_management/me"; // static method
+const meUsernameAPI = "api/user_management/me_username"; // static method
 const userIdAPI = "/api/user_management/user/id/" // dynamic method -> add user_id
-const userViewAPI = "/api/user_management/user/"; // dynamic method -> add user_id from window.location.pathname.split('/')[2]
+const userUsernameAPI = "/api/user_management/user/username/" // dynamic method -> add user_id
 const userSearchAPI = "/api/user_management/search/"; // dynamic method -> add search_query
 const editProfileAPI = "/api/user_management/edit_profile"; // static method
-const addFriendAPI = "/api/user_management/add_friend/"; // dynamic method -> add user_id
+const avatarUploadAPI = "/api/user_management/upload_avatar"; // static method
+const getFriendsAPI = "/api/user_management/get_friends"; // static method
+const getOnlineStatusAPI = "/api/user_management/get_online_status/"; // dynamic method -> add username
 const friendshipStatusAPI = "/api/user_management/friends/"; // dynamic method -> add user_id
+const addFriendAPI = "/api/user_management/add_friend/"; // dynamic method -> add user_id
+const removeFriendAPI = "/api/user_management/remove_friend/"; // dynamic method -> add user_id
+const searchUsersAPI = "/api/user_management/user/search/"; // dynamic method -> add search_query
+const findMatchAPI = "/api/user_management/find_match/"; // dynamic method -> add username
 
-// Notoifications
+
+// Notifications
 const setSeenNotifsAPI = "/api/notif/set_seen"; // static method
 const getAllNotifsAPI = "/api/notif/get_notifs/all"; // static method
 const getUnseenNotifsAPI = "/api/notif/get_notifs/unseen"; // static method
