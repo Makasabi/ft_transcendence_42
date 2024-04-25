5ed3a0d5d2192b282c8730535201ac762cf5e110
mrony <marie.rony@gmail.com>
2024-04-25 19:02:22 +0200

    🚀 Implement local tournament waiting room front - WIP

--- web/nginx/html/front/pages/room/WaitingRoom.js
+++ web/nginx/html/front/pages/room/WaitingRoom.js
@@ -29,6 +29,7 @@ export class WaitingRoomView extends IView {
 	{
 		return route === "/waiting_room";
 	}
+
 	async render()
 	{
 		const schedule = JSON.parse(localStorage.getItem('schedule'));
@@ -49,13 +50,17 @@ export class WaitingRoomView extends IView {
 		}
 		const round = state.currentRound;
 		const match = state.currentGame;
-
+		
 		let html = await fetch("/front/pages/room/WaitingRoom.html")
-			.then(response => response.text())
+		.then(response => response.text())
+		document.querySelector("main").innerHTML = html;
+
+		drawLocalRounds(schedule, state);
+		displayCurrentRoundGames(schedule, state);
+
 		html = html.replace("{{round}}", round);
 		html = html.replace("{{player1}}", match.players[0]);
 		html = html.replace("{{player2}}", match.players[1]);
-		document.querySelector("main").innerHTML = html;
 
 		document.getElementById('start_game').addEventListener('click', async e => {
 			e.preventDefault();
@@ -99,16 +104,70 @@ export class WaitingRoomView extends IView {
 
 export function getCurrentRoundAndGame(schedule)
 {
-    for (let i = 0; i < schedule.length; i++)
+	for (let i = 0; i < schedule.length; i++)
 	{
-        const round = schedule[i];
-        
-        for (let j = 0; j < round.matches.length; j++)
+		const round = schedule[i];
+		
+		for (let j = 0; j < round.matches.length; j++)
 		{
-            const match = round.matches[j];
-			if (match.winner === null && !match.players.some(str => str === null))
-                return { currentRound: round.id, currentGame: match };
-        }
-    }
-    return null;
+			const match = round.matches[j];
+			if (match.winner === null || match.winner === undefined)
+				return { currentRound: round.id, currentGame: match };
+		}
+	}
+	return null;
+}
+
+function drawLocalRounds(schedule, state)
+{
+	const current_round = state.currentRound;
+
+	let rounds = document.getElementById('local_rounds_map');
+
+	console.log("rounds : ", rounds);
+
+	for (let i = 0; i < schedule.length; i++)
+	{
+		const round = schedule[i];
+		const round_div = document.createElement('div');
+		round_div.classList.add('round_style');
+		const round_nu = document.createElement('h3');
+		round_nu.innerHTML = round.id;
+		round_div.id = `round${round.id}`;
+		const round_img = document.createElement('img');
+		round_img.src = "/front/ressources/img/svg/hexagon.svg";
+		if (round.id === current_round){
+			round_div.classList.add('current_round');
+			round_nu.style.color = "var(--contrast)";
+		}
+		
+		round_div.appendChild(round_nu);
+		round_div.appendChild(round_img);
+		rounds.appendChild(round_div);
+	}
+}
+
+function displayCurrentRoundGames(schedule, state) {
+	// displays the details of the current round in a table format
+	// the current round is the one diosplayed in the div.
+	// an event listener is on the round images to display the games of the round
+
+	let tablebody = document.getElementById('local_tour_game_table')
+	const current_round = state.currentRound;
+
+	const round = schedule[current_round]
+	console.log("round : ", round);
+
+	let table = '';
+	for (let i = 0; i < round.matches.length; i++)
+	{
+		console.log("match : ", round.matches[i]);
+		const game = round.matches[i];
+		table += `<tr id="game_${game.id}">`;
+		table += `<td>${game.id}</td>`;
+		table += `<td>${game.players[0]}</td>`;
+		table += `<td>${game.players[1]}</td>`;
+		table += `</tr>`;
+	}
+	tablebody.innerHTML = table;
 }
